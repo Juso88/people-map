ac6b4f361b3616279c0a0e5d6a502422952ed20a
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2025-05-13 01:38:54 -0700

    add update connection code paths + selected node to graphcanvas

--- frontend/src/components/GraphCanvas.jsx
+++ frontend/src/components/GraphCanvas.jsx
@@ -3,11 +3,14 @@ import GraphTitle from './GraphTitle';
 import GraphDisplay from './GraphDisplay';
 import ButtonPanel from './ButtonPanel';
 import OverlayInput from './OverlayInput';
-import { createPerson, createConnection, deleteConnectionByName, createPersonAndConnect } from '../features/api';
+import { createPerson, createConnection, updateConnection, deleteConnectionByName, createPersonAndConnect } from '../features/api';
+import InfoBox from './InfoBox';
 
 const GraphCanvas = ({ graphData, username, refreshGraph }) => {
   const [inputMode, setInputMode] = useState(null); // 'add' or 'remove'
   const [inputValue, setInputValue] = useState('');
+  const [selectedNode, setSelectedNode] = useState(null);
+  
 
   const handleAdd = () => {
     setInputMode('add');
@@ -16,6 +19,11 @@ const GraphCanvas = ({ graphData, username, refreshGraph }) => {
 
   const handleRemove = () => {
     setInputMode('remove');
+    setInputValue('');
+  };
+
+  const handleUpdate = () => {
+    setInputMode('update');
     setInputValue('');
   };
 
@@ -34,15 +42,41 @@ const GraphCanvas = ({ graphData, username, refreshGraph }) => {
       await deleteConnectionByName(inputValue);
     }
 
+    //if the update comes from the button, we should update connection.
+    //if the update comes from the info box, we should update person.
+    if (inputMode === 'update') {
+      const [oldTargetId, newTargetId] = inputValue.split(' ');
+      await updateConnection(username, oldTargetId, newTargetId);
+    }
+
     setInputMode(null);
     await refreshGraph();
   };
+  const handleNodeClick = (node) => {
+    setSelectedNode(node);
+    if (node) {showInfoBox();}
+    else {setSelectedNode(null);}
+    return;
+  };
+
+  const showInfoBox = () => {
+    if (!selectedNode) return null;
+    
+    return (
+      <InfoBox
+        value={selectedNode.description}
+        onSubmit={handleSubmit}
+        onCancel={() => setSelectedNode(null)}
+        personName={selectedNode.name}
+      />
+    );
+  }
 
   return (
     <div style={{ width: '100%', height: '100%', position: 'relative' }}>
       <GraphTitle rootName={username}/>
-      <GraphDisplay graphData={graphData} />
-      <ButtonPanel onAdd={handleAdd} onRemove={handleRemove} />
+      <GraphDisplay graphData={graphData} nodeClickedFunction={handleNodeClick} />
+      <ButtonPanel onAdd={handleAdd} onRemove={handleRemove} onUpdate={handleUpdate} selectedNodeText={selectedNode} />
 
       {inputMode && (
         <OverlayInput
